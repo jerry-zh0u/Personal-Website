6e340543626eb91532e2b08271557c3301fe774e
jerry-zh0u <152764900+jerry-zh0u@users.noreply.github.com>
2026-01-07 15:31:18 -0800

    content added

--- home.js
+++ home.js
@@ -11,10 +11,9 @@ function setupFEBImageSlide(){
         {src: "images/segment-board-bottom-layer.png", description: "Segment Board 4th Layer (Signal Traces)"}
     ];
     // const febSlide = document.getElementById("slideshow");
-    const febSlide = document.querySelector(".slideshow")
+    const febSlide = document.querySelector(".feb-slideshow")
     const febImg = febSlide.querySelector(".img-slideshow");
-    // const febText = document.querySelector(".description-slideshow")
-    // const febImg = document.getElementById("img-slideshow");
+    const febText = febSlide.querySelector(".description-slideshow")
 
     const imgNext = febSlide.querySelector(".right");
     const imgPrev = febSlide.querySelector(".left");
@@ -25,7 +24,7 @@ function setupFEBImageSlide(){
         febImageIndex = index
 
         febImg.src = febImageList[index].src;
-        // febText.textContent = febImageList[index].description;
+        febText.textContent = febImageList[index].description;
 
         if(febImageIndex === 0){
             imgPrev.classList.add("hidden");
@@ -44,6 +43,88 @@ function setupFEBImageSlide(){
     imgPrev.onclick = () => updateIndex(febImageIndex - 1);
 
     updateIndex(febImageIndex)
+}
+
+function setupESBImageSlide(){
+    const esbImageList = [
+        {src: "images/RPM-real.jpg", description: "Physical Random Positioning Machine"},
+        {src: "images/RPM-3d-view.png", description: "RPM 3d-view"},
+        {src: "images/RPM-layer1.png", description: "RPM Board 1st Layer (Power Traces + Signal Traces)"},
+        {src: "images/RPM-layer2.png", description: "RPM Board 2nd Layer (Signal Traces)"}
+    ];
+    // const febSlide = document.getElementById("slideshow");
+    const esbSlide = document.querySelector(".esb-slideshow")
+    const esbImg = esbSlide.querySelector(".img-slideshow");
+    const esbText = esbSlide.querySelector(".description-slideshow")
+
+    const imgNext = esbSlide.querySelector(".right");
+    const imgPrev = esbSlide.querySelector(".left");
+
+    let esbImageIndex = 0;
+
+    function updateIndex(index){
+        esbImageIndex = index
+
+        esbImg.src = esbImageList[index].src;
+        esbText.textContent = esbImageList[index].description;
+
+        if(esbImageIndex === 0){
+            imgPrev.classList.add("hidden");
+        }else{
+            imgPrev.classList.remove("hidden");
+        }
+
+        if(esbImageIndex === esbImageList.length - 1){
+            imgNext.classList.add("hidden");
+        }else{
+            imgNext.classList.remove("hidden");
+        }
+    }
+
+    imgNext.onclick = () => updateIndex(esbImageIndex + 1);
+    imgPrev.onclick = () => updateIndex(esbImageIndex - 1);
+
+    updateIndex(esbImageIndex)
+}
+
+function setupHSImageSlide(){
+    const hsImageList = [
+        {src: "images/robotics-team-pic.jpg", description: ""},
+        {src: "images/robotics-robot.jpg", description: ""},
+    ];
+    // const febSlide = document.getElementById("slideshow");
+    const hsSlide = document.querySelector(".hs-slideshow")
+    const hsImg = hsSlide.querySelector(".img-slideshow");
+    const hsText = hsSlide.querySelector(".description-slideshow")
+
+    const imgNext = hsSlide.querySelector(".right");
+    const imgPrev = hsSlide.querySelector(".left");
+
+    let hsImageIndex = 0;
+
+    function updateIndex(index){
+        hsImageIndex = index
+
+        hsImg.src = hsImageList[index].src;
+        hsText.textContent = hsImageList[index].description;
+
+        if(hsImageIndex === 0){
+            imgPrev.classList.add("hidden");
+        }else{
+            imgPrev.classList.remove("hidden");
+        }
+
+        if(hsImageIndex === hsImageList.length - 1){
+            imgNext.classList.add("hidden");
+        }else{
+            imgNext.classList.remove("hidden");
+        }
+    }
+
+    imgNext.onclick = () => updateIndex(hsImageIndex + 1);
+    imgPrev.onclick = () => updateIndex(hsImageIndex - 1);
+
+    updateIndex(hsImageIndex)
 }
 
 function cowAnimate(){
@@ -104,5 +185,7 @@ function fetchLastUpdated() {
 document.addEventListener("DOMContentLoaded", fetchLastUpdated);
 document.addEventListener("DOMContentLoaded", cowAnimate)
 document.addEventListener("DOMContentLoaded", setupFEBImageSlide)
+document.addEventListener("DOMContentLoaded", setupESBImageSlide)
+document.addEventListener("DOMContentLoaded", setupHSImageSlide)
 
 document.getElementById("year").textContent = new Date().getFullYear();
